68ca728c6736ea6c902f1b717274d955b0de3ab2
Fabio-F16 <fipsfabio95@gmail.com>
2022-03-15 22:23:12 +0100

    esercizio completato più bonus..?

--- script.js
+++ script.js
@@ -1,47 +1,98 @@
 // console.log('js okk')
 
 /* creare una griglia di gioco quadrata, 
-in cui ogni cella contiene un numero tra quelli compresi in un range compreso tra 1 e 100
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.*/
+in cui ogni cella contiene un numero tra quelli compresi in un range compreso tra 1 e 100.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+Tre difficoltà:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49 
+*/
 
-
-
-
+const difficoltàUno = document.querySelector('.difficoltàUno');
+const difficoltàDue = document.querySelector('.difficoltàDue');
+const difficoltàTre = document.querySelector('.difficoltàTre');
 
 const square = document.getElementById('square');
 
-const columns = 10;
-const row = 10;
+// difficoltà uno
+difficoltàUno.addEventListener('click', function() {
 
-const totalCells = columns * row;
+    const columns = 10;
+    const row = 10;
+    const totalCells = columns * row;
 
+    for(let i = 1; i < totalCells + 1; i++){
 
-for(let i = 1; i < totalCells + 1; i++){
+    const cell = createCell();
+    cell.classList.add('cell-10');
+    const number = i
+    cell.innerText = number;
+    square.appendChild(cell);
 
-const cell = createCell();
-const number = i
-cell.innerText = number;
-square.appendChild(cell);
-
-cell.addEventListener('click', function() {
-
-    cell.classList.toggle('bg-color-aqua');
-
+    cell.addEventListener('click', function() {
+        cell.classList.toggle('bg-color-aqua');
+    })
+    }
 })
-}
+// fine difficoltà uno
 
 
+// difficoltà due
+difficoltàDue.addEventListener('click', function() {
+
+    const columns = 9;
+    const row = 9;
+    const totalCells = columns * row;
+
+    for(let i = 1; i < totalCells + 1; i++){
+
+    const cell = createCell();
+    cell.classList.add('cell-9');
+    const number = i
+    cell.innerText = number;
+    square.appendChild(cell);
+
+    cell.addEventListener('click', function() {
+        cell.classList.toggle('bg-color-aqua');
+    })
+    }
+})
+// fine difficoltà due
+
+
+// difficoltà tre
+difficoltàTre.addEventListener('click', function() {
+
+    const columns = 7;
+    const row = 7;
+    const totalCells = columns * row;
+
+    for(let i = 1; i < totalCells + 1; i++){
+
+    const cell = createCell();
+    cell.classList.add('cell-7');
+    const number = i
+    cell.innerText = number;
+    square.appendChild(cell);
+
+    cell.addEventListener('click', function() {
+        cell.classList.toggle('bg-color-aqua');
+    })
+    }
+})
+// fine difficoltà tre
 
 
 // funzioni
 
-// genero un numero random
+// genero un numero random (non utilizzata)
 function generateNumber(min, max){
     const range = (max - min) + 1;
     const numeroRandom = Math.floor(Math.random()*range + min);
     return numeroRandom;
 }
-// fine genero un numero random
+// fine genero un numero random (non utilizzata)
 
 // creo una cella nel DOM
 function createCell(){
@@ -50,3 +101,5 @@ function createCell(){
     return cell;
 }
 // fine creo una cella nel DOM
+
+
