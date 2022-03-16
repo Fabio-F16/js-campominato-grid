bc0f96a08f1b9071743602f4ce36c78480dba2bc
Fabio-F16 <fipsfabio95@gmail.com>
2022-03-16 16:56:25 +0100

    fine prima parte

--- script.js
+++ script.js
@@ -9,89 +9,16 @@ con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49 
 */
 
+// chiamo bottoni genera griglia
 const difficoltàUno = document.querySelector('.difficoltàUno');
 const difficoltàDue = document.querySelector('.difficoltàDue');
 const difficoltàTre = document.querySelector('.difficoltàTre');
 
 
-
-// difficoltà uno
-difficoltàUno.addEventListener('click', () => {
-    const square = document.getElementById('square');
-    square.innerHTML = '';
-
-    const columns = 10;
-    const row = 10;
-    const totalCells = columns * row;
-
-    for(let i = 1; i < totalCells + 1; i++){
-
-        const cell = createCell();
-        cell.classList.add('cell-10');
-        const number = i
-        cell.innerText = number;
-        square.appendChild(cell);
-
-        cell.addEventListener('click', function() {
-            cell.classList.toggle('bg-color-aqua');
-        })
-    }
-})
-// fine difficoltà uno
-
-
-// difficoltà due
-difficoltàDue.addEventListener('click', () => {
-
-    const square = document.getElementById('square');
-    square.innerHTML = '';
-
-    const columns = 9;
-    const row = 9;
-    const totalCells = columns * row;
-
-    for(let i = 1; i < totalCells + 1; i++){
-
-        const cell = createCell();
-        cell.classList.add('cell-9');
-        const number = i
-        cell.innerText = number;
-        square.appendChild(cell);
-
-        cell.addEventListener('click', function() {
-            cell.classList.toggle('bg-color-aqua');
-        })
-    }
-})
-// fine difficoltà due
-
-
-// difficoltà tre
-difficoltàTre.addEventListener('click', () => {
-
-    const square = document.getElementById('square');
-    square.innerHTML = '';
-
-    const columns = 7;
-    const row = 7;
-    const totalCells = columns * row;
-
-    for(let i = 1; i < totalCells + 1; i++){
-
-        const cell = createCell();
-        cell.classList.add('cell-7');
-        const number = i
-        cell.innerText = number;
-        square.appendChild(cell);
-
-        cell.addEventListener('click', function() {
-            cell.classList.toggle('bg-color-aqua');
-        })
-    }
-})
-// fine difficoltà tre
-
-
+// applico ai bottoni l'evento click per la creazione griglia con parametri
+difficoltàUno.addEventListener('click', () => creazioneCampo(100, 'cell-10'));
+difficoltàDue.addEventListener('click', () => creazioneCampo(81, 'cell-9'));
+difficoltàTre.addEventListener('click', () => creazioneCampo(49, 'cell-7'));
 
 
 
@@ -100,19 +27,41 @@ difficoltàTre.addEventListener('click', () => {
 // funzioni
 
 // genero un numero random (non utilizzata)
-function generateRandomNumber(min, max){
+function generateRandomNumber(min, max) {
     const range = (max - min) + 1;
-    const numeroRandom = Math.floor(Math.random()*range + min);
+    const numeroRandom = Math.floor(Math.random() * range + min);
     return numeroRandom;
 }
 // fine genero un numero random (non utilizzata)
 
 // creo una cella nel DOM
-function createCell(){
+function createCell() {
     const cell = document.createElement('div');
     cell.classList.add('cell');
     return cell;
 }
 // fine creo una cella nel DOM
 
+// funzione creazione griglia
+function creazioneCampo(totalCells, levelClass) {
 
+    const square = document.getElementById('square');
+    square.innerHTML = '';
+
+    for (let i = 1; i <= totalCells; i++) {
+
+        const cell = createCell();
+        cell.classList.add(levelClass);
+
+        let number = i
+        cell.innerText = number;
+        square.appendChild(cell);
+
+        cell.addEventListener('click', () => cell.classList.toggle('bg-color-aqua'));
+
+    }
+
+}
+// fine funzione creazione griglia
+
+// fine funzioni
